0192610763e6dfd3453d4f35f8bce7dcd784f299
zach lewis <zlewis@clarityinnovates.com>
2021-10-18 15:15:54 -0500

    fix indent

--- src/python/foo.py
+++ src/python/foo.py
@@ -1,17 +1,16 @@
 # Selective copy
 
-'''Walking through a folder tree and searching for files with a certain file extension('.jpg','.pdf')
- and copying from whatever location they are present at to a new folder'''
+"""Walking through a folder tree and searching for files with a certain file extension('.jpg','.pdf')
+ and copying from whatever location they are present at to a new folder"""
 
- import shutil
+import shutil
 
- import os
+import os
 
- # Walking through a folder tree
- os.makedirs('newfolder', exist_ok=True)
+# Walking through a folder tree
+os.makedirs("newfolder", exist_ok=True)
 
- for folders,subfoldes,filenames in os.walk('.'):
- 	for filename in filenames:
- 		if filename.endswith('.jpg') or filename.endswith('.pdf'):
- 			shutil.copy(file_name,'newfolder')
- 			
+for folders, subfoldes, filenames in os.walk("."):
+    for filename in filenames:
+        if filename.endswith(".jpg") or filename.endswith(".pdf"):
+            shutil.copy(file_name, "newfolder")
